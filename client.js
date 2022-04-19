75ec23678d0153dbcb02c5a33f997a112c50cf02
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-19 12:05:50 -0500

    Exports connect function. File now sends a 3 letter name to the server (SNK)

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  })
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  })
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: SNK')
+  })
+  return conn;
+}
+
+module.exports = {
+  connect
+};
