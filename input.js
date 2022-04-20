105e30b8ef67e89be358fa50ec8a48c081c1d21d
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-20 09:23:55 -0500

    Added pre-written messages that can be posted using 1, 2 and 3 keys

--- input.js
+++ input.js
@@ -16,6 +16,16 @@ const handleUserInput = (key) => {
   if (key === 'd') {
     connection.write('Move: right');
   }
+
+  if (key === '1') {
+    connection.write('Say: That was easy');
+  }
+  if (key === '2') {
+    connection.write('Say: Blocked!!');
+  }
+  if (key === '3') {
+    connection.write('Say: You got lucky');
+  }
 };
 
 const setupInput = function(conn) {
