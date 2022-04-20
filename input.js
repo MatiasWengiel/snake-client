ee48ae92130096330eaa640e482025b0dab90098
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-20 11:14:03 -0500

    Modified file so that the snake moves in the last direction that was indicated at a set interval of 1000 ms. This is not part of the original assignment, but a self-directed 'stretch' goal as I wanted to practice intervals AND make the snake game closer to the original

--- input.js
+++ input.js
@@ -9,22 +9,44 @@ const {
 } = require('./constants');
 
 let connection;
+let movement;
+
+
 const handleUserInput = (key) => {
+ 
   const inputObj = {
     [UP_KEY]: 'Move: up',
     [LEFT_KEY]: 'Move: left',
     [DOWN_KEY]: 'Move: down',
     [RIGHT_KEY]: 'Move: right',
+
+  }
+
+  const messageObj = {
     [MESSAGE_ONE]: 'Say: That was easy',
     [MESSAGE_TWO]: 'Say: Blocked!!',
-    [MESSAGE_THREE]: 'Say: You got lucky',
+    [MESSAGE_THREE]: 'Say: You got lucky'
   }
 
-  if (key === '\u0003') {
+  // Determines type of input from key and responds accordingly with movement, message, etc
+  if (inputObj[key]) {
+
+    if(movement) {
+      clearInterval(movement);
+    }
+  
+    movement = setInterval(() => {
+      connection.write(inputObj[key]);
+    }, 1000);
+
+  } else if (messageObj[key]) {
+    connection.write(messageObj[key])
+  } else if (key === '\u0003'){
     process.exit();
+  } else {
+    console.log('Invalid key');
   }
 
-    connection.write(inputObj[key]);
 };
 
 const setupInput = function(conn) {
