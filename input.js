5214af3aa4f7607bc6be2e06e22ded629247ccc9
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-20 10:06:13 -0500

    Modified input file to use an object for mapping which action is sent to the server, instead of using if statements. Also modified file to require and use constants for key mappings, from the constants file

--- input.js
+++ input.js
@@ -1,31 +1,30 @@
+const {
+  UP_KEY,
+  LEFT_KEY,
+  DOWN_KEY,
+  RIGHT_KEY,
+  MESSAGE_ONE,
+  MESSAGE_TWO,
+  MESSAGE_THREE,
+} = require('./constants');
 
 let connection;
 const handleUserInput = (key) => {
+  const inputObj = {
+    [UP_KEY]: 'Move: up',
+    [LEFT_KEY]: 'Move: left',
+    [DOWN_KEY]: 'Move: down',
+    [RIGHT_KEY]: 'Move: right',
+    [MESSAGE_ONE]: 'Say: That was easy',
+    [MESSAGE_TWO]: 'Say: Blocked!!',
+    [MESSAGE_THREE]: 'Say: You got lucky',
+  }
+
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
-    connection.write('Move: up');
-  }
-  if (key === 'a') {
-    connection.write('Move: left');
-  }
-  if (key === 's') {
-    connection.write('Move: down');
-  }
-  if (key === 'd') {
-    connection.write('Move: right');
-  }
 
-  if (key === '1') {
-    connection.write('Say: That was easy');
-  }
-  if (key === '2') {
-    connection.write('Say: Blocked!!');
-  }
-  if (key === '3') {
-    connection.write('Say: You got lucky');
-  }
+    connection.write(inputObj[key]);
 };
 
 const setupInput = function(conn) {
