091be012b0cd15fdf1141b9b34176baf58b01a60
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-20 08:53:47 -0500

    fixed linting errors

--- client.js
+++ client.js
@@ -4,22 +4,22 @@ const connect = () => {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
-  })
+  });
 
   conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
-  })
+  });
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: SNK');
     // conn.write('Move: up');
     // setInterval( () => conn.write('Move: up'), 50)
-  })
+  });
   return conn;
-}
+};
 
 module.exports = {
   connect
