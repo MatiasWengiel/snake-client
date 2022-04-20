21a3883717c87c2615089c8b7a8fd1d9289a8efc
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-20 10:06:28 -0500

    Created file that holds constants for key mappings, IP and PORT

---
+++ constants.js
@@ -0,0 +1,22 @@
+const IP = 'localhost';
+const PORT = 50541
+
+const UP_KEY = 'w';
+const LEFT_KEY = 'a';
+const DOWN_KEY = 's';
+const RIGHT_KEY = 'd';
+const MESSAGE_ONE = '1';
+const MESSAGE_TWO = '2';
+const MESSAGE_THREE = '3';
+
+module.exports = {
+  IP,
+  PORT,
+  UP_KEY,
+  LEFT_KEY,
+  DOWN_KEY,
+  RIGHT_KEY,
+  MESSAGE_ONE,
+  MESSAGE_TWO,
+  MESSAGE_THREE
+}
